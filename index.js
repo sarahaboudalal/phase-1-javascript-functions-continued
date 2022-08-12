d4b0c85803c8908c67212a75dc4a3dc8008ed69f
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-08-12 17:34:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(work = "go to the office"){
+ return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(result) {
+    return function inner(adj) {
+    return `You are ${result}${adj}${result}!`
+    };
+}
+
